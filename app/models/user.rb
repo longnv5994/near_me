befbb5d056980412f711746e2dac8ba23550b7bb
longnv5994 <longnv5994@gmail.com>
2016-10-04 09:29:58 +0700

    login-fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ApplicationRecord
   has_many :user_locations
   has_many :relationships
   has_many :friends, through: :relationships
+
+  def self.from_omniauth auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.image = auth.info.image
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- db/migrate/20160929020226_create_users.rb
+++ db/migrate/20160929020226_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :provider
       t.string :uid
       t.string :oauth_token
+      t.string :image
+      t.datetime :oauth_expires_at
       t.boolean :signed_in
 
       t.timestamps
