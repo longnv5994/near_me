4a76d87b4dfe5dde2bf57c3f92e87821d5e0c94e
tuyen94 <tuyenmary@gmail.com>
2016-10-18 10:55:37 +0700

    fix-craw-all-location

--- app/workers/craw_location.rb
+++ app/workers/craw_location.rb
@@ -11,41 +11,48 @@ class CrawLocation
 
   private
   def craw_location type, id
+    locations = []
     new_graph
-    @graph_data = @graph.get_connections("me", "friends", fields: [type.to_s+"{place}"], limit: 1000)
-    @graph_data.each do |data|
+    @data = @graph.get_connections("me", "friends", fields: [type.to_s+"{place}"], limit: 5000)
+    @data.each do |data|
       if data[type]
         craw_friends data["id"]
         create_relationship id, data["id"]
-        data[type]["data"].each do |data_location|
-          if data_location["place"]
-            if data_location["place"]["location"]
-              @location = Location.find_or_create_by(city:  data_location["place"]["location"]["city"],
-                country:  data_location["place"]["location"]["country"],
-                place_name: data_location["place"]["name"],
-                lat: data_location["place"]["location"]["latitude"],
-                long: data_location["place"]["location"]["longitude"],
-                street: data_location["place"]["location"]["street"],
-                zip: data_location["place"]["location"]["zip"])
-                @location.save
-                @location.update_attributes(user_locations_attributes:
-                  [user_id: find_friend(data["id"]).id])
+        @graph_data = @graph.get_connections(data["id"], type, fields: ["place"], limit: 5000)
+        @graph_data.each do |graph_data|
+          if graph_data["place"]
+            if graph_data["place"]["location"]
+              @location = Location.find_or_create_by(city:  graph_data["place"]["location"]["city"],
+                country: graph_data["place"]["location"]["country"],
+                place_name: graph_data["place"]["name"],
+                lat: graph_data["place"]["location"]["latitude"],
+                long: graph_data["place"]["location"]["longitude"],
+                street: graph_data["place"]["location"]["street"],
+                zip: graph_data["place"]["location"]["zip"])
+              @location.update_attributes(user_locations_attributes:
+                [user_id: find_friend(data["id"]).id])
+              locations << @location
             end
           end
         end
       end
     end
+    Location.import locations, recursive: true, batch_size: 500
+    User.import @users
+    Relationship.import @relationships
   end
 
   def craw_friends friend_uid
-    @user_new = User.find_or_create_by uid: friend_uid
-    @user_new.save
+    @users = []
+    user_new = User.find_or_create_by uid: friend_uid
+    @users << user_new
   end
 
   def create_relationship user_id, friend_uid
+    @relationships = []
     find_friend friend_uid
-    @relationship = Relationship.find_or_create_by(user_id: user_id, friend_id: @friend.id )
-    @relationship.save
+    relationship = Relationship.find_or_create_by(user_id: user_id, friend_id: @friend.id )
+    @relationships << relationship
   end
 
   def new_graph
