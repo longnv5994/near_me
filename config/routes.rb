3bd40f89afcc99fc78c688c961bc9dd82f5929bf
longnv5994 <longnv5994@gmail.com>
2016-10-19 17:18:05 +0700

    Get current location

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "auth/:provider/callback", to: "sessions#create"
   get "auth/failure", to: redirect("/")
   get "signout", to: "sessions#destroy", as: "signout"
+  post "locations/get_position", to: "locations#get_position"
   resource :sessions, only: [:new, :create, :destroy]
 end
